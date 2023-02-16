e504d5df380c6e8ca4be78ff151ae88f2266033e
Luismarin2510 <opraedu@gmail.com>
2023-02-15 21:50:37 -0500

    agregando titular

--- src/proyectobank/crear_cuenta.java
+++ src/proyectobank/crear_cuenta.java
@@ -5,7 +5,8 @@ public class crear_cuenta {
 	public static void main(String[] args) {
 		cuenta primeracuenta = new cuenta();
 		primeracuenta.saldo = 1000;
-		System.out.println(primeracuenta.saldo);
+		primeracuenta.titular = "luis";
+		System.out.println(primeracuenta.saldo + primeracuenta.titular);
 
 	}
 
