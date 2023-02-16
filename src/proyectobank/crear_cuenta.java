de0f29aa950b5b0c4dc673021012302d930af52f
Luismarin2510 <opraedu@gmail.com>
2023-02-15 21:39:53 -0500

    cracion de objeto

--- src/proyectobank/crear_cuenta.java
+++ src/proyectobank/crear_cuenta.java
@@ -3,7 +3,9 @@ package proyectobank;
 public class crear_cuenta {
 
 	public static void main(String[] args) {
-		
+		cuenta primeracuenta = new cuenta();
+		primeracuenta.saldo = 1000;
+		System.out.println(primeracuenta.saldo);
 
 	}
 
